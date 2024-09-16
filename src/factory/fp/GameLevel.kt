f603282fe93917c3a62cf7e51354b056f5624841
JYC11 <jaeyoon.c@gmail.com>
2024-09-16 19:48:18 +0900

    more complicated fp factory example

--- src/factory/fp/GameLevel.kt
+++ src/factory/fp/GameLevel.kt
@@ -1,94 +1,151 @@
 package factory.fp
 
 
-data class GameLevel(
-    var walls: String? = null,
-    var floors: String? = null,
-    var enemies: String? = null,
-    var loot: String? = null,
+data class Wall(
+    val material: String,
+    val color: String,
+    val secretePathCount: Int,
+)
+
+data class Floor(
+    val material: String,
+    val color: String,
+    val sound: String,
+)
+
+data class Mob(
+    val name: String,
+    val hp: Double,
+    val attack: Double,
+    val defense: Double,
+    val uniqueLoot: String,
+)
+
+data class Loot(
+    val name: String,
+    val value: Int
 )
 
 
-typealias SetWalls = (level: GameLevel) -> GameLevel
-typealias SetFloors = (level: GameLevel) -> GameLevel
-typealias SetEnemies = (level: GameLevel) -> GameLevel
-typealias SetLoot = (level: GameLevel) -> GameLevel
+data class GameLevel(
+    var walls: Wall? = null,
+    var floors: Floor? = null,
+    var mobs: Mob? = null,
+    var loot: Loot? = null,
+)
 
-typealias GameLevelMaker = (SetWalls, SetFloors, SetEnemies, SetLoot) -> GameLevel
 
-val makeFantasyGameLevel: GameLevelMaker = { wallSetter, floorSetter, enemySetter, lootSetter ->
+typealias WallFactory = (level: GameLevel) -> GameLevel
+typealias FloorFactory = (level: GameLevel) -> GameLevel
+typealias MobFactory = (level: GameLevel) -> GameLevel
+typealias LootFactory = (level: GameLevel) -> GameLevel
+
+typealias GameLevelFactory = (WallFactory, FloorFactory, MobFactory, LootFactory) -> GameLevel
+
+val makeFantasyGameLevel: GameLevelFactory = { wallFactory, floorFactory, mobFactory, lootFactory ->
     var level = GameLevel()
-    level = wallSetter(level)
-    level = floorSetter(level)
-    level = enemySetter(level)
-    level = lootSetter(level)
+    level = wallFactory(level)
+    level = floorFactory(level)
+    level = mobFactory(level)
+    level = lootFactory(level)
     level
 }
 
-val setStoneWalls: SetWalls = { level ->
+val setStoneWalls: WallFactory = { level ->
     GameLevel(
-        walls = "stone",
+        walls = Wall(
+            material = "Stone",
+            color = "Grey",
+            secretePathCount = 5,
+        ),
         floors = level.floors,
-        enemies = level.enemies,
+        mobs = level.mobs,
         loot = level.loot,
     )
 }
-val setWoodWalls: SetWalls = { level ->
+val setWoodWalls: WallFactory = { level ->
     GameLevel(
-        walls = "wood",
+        walls = Wall(
+            material = "Wood",
+            color = "Brown",
+            secretePathCount = 3,
+        ),
         floors = level.floors,
-        enemies = level.enemies,
+        mobs = level.mobs,
         loot = level.loot,
     )
 }
 
-val setStoneTiles: SetFloors = { level ->
+val setStoneTiles: FloorFactory = { level ->
     GameLevel(
         walls = level.walls,
-        floors = "stone",
-        enemies = level.enemies,
+        floors = Floor(
+            material = "Stone",
+            color = "Light Grey",
+            sound = "stone_stepping_sound.mp4"
+        ),
+        mobs = level.mobs,
         loot = level.loot,
     )
 }
-val setDirtTiles: SetFloors = { level ->
+val setDirtTiles: FloorFactory = { level ->
     GameLevel(
         walls = level.walls,
-        floors = "dirt",
-        enemies = level.enemies,
-        loot = level.loot,
-    )
-}
-
-val spawnImps: SetEnemies = { level ->
-    GameLevel(
-        walls = level.walls,
-        floors = level.floors,
-        enemies = "imp",
-        loot = level.loot,
-    )
-}
-val spawnWolves: SetWalls = { level ->
-    GameLevel(
-        walls = level.walls,
-        floors = level.floors,
-        enemies = "wolf",
+        floors = Floor(
+            material = "Stone",
+            color = "Light Brown",
+            sound = "dirt_stepping_sound.mp4"
+        ),
+        mobs = level.mobs,
         loot = level.loot,
     )
 }
 
-val setNoLoot: SetLoot = { level ->
+val spawnImps: MobFactory = { level ->
     GameLevel(
         walls = level.walls,
         floors = level.floors,
-        enemies = level.enemies,
+        mobs = Mob(
+            name ="Imp",
+            hp = 30.0,
+            attack = 15.0,
+            defense = 10.0,
+            uniqueLoot = "imp wing"
+        ),
+        loot = level.loot,
+    )
+}
+val spawnWolves: WallFactory = { level ->
+    GameLevel(
+        walls = level.walls,
+        floors = level.floors,
+        mobs = Mob(
+            name ="Wolf",
+            hp = 20.0,
+            attack = 20.0,
+            defense = 5.0,
+            uniqueLoot = "wolf tooth"
+        ),
+        loot = level.loot,
+    )
+}
+
+val setNoLoot: LootFactory = { level ->
+    GameLevel(
+        walls = level.walls,
+        floors = level.floors,
+        mobs = level.mobs,
         loot = null,
     )
 }
-val setGold: SetLoot = { level ->
+val setGold: LootFactory = { level ->
     GameLevel(
         walls = level.walls,
         floors = level.floors,
-        enemies = level.enemies,
-        loot = "gold",
+        mobs = level.mobs,
+        loot = Loot(
+            name = "Gold",
+            value = 50
+        ),
     )
 }
