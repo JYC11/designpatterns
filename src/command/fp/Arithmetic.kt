870d6b53637f1159c5e8c244adaaaad3a87e4c87
JYC11 <jaeyoon.c@gmail.com>
2024-09-21 12:49:47 +0900

    command fp example change

--- src/command/fp/Arithmetic.kt
+++ src/command/fp/Arithmetic.kt
@@ -2,8 +2,8 @@ package command.fp
 
 typealias Command = (Int) -> Int
 
-val increment: Command = { it + 1 }
+val increment: Command = { num -> num + 1 }
 
-val decrement: Command = { it - 1 }
+val decrement: Command = { num -> num - 1 }
 
 fun execute(command: Command, value: Int): Int = command(value)
